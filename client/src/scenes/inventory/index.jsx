e51862f2bb05c9d78813ec7057e3c12c146ba94d
Dananjaya_Rajarathna <83546013+123ddr@users.noreply.github.com>
2024-04-21 12:43:27 +0530

    update(4/21)

--- client/src/scenes/inventory/index.jsx
+++ client/src/scenes/inventory/index.jsx
@@ -813,7 +813,7 @@ const Inventory = () => {
             <DataGrid
               loading={isLoading || !data}
               getRowId={(row) => row._id}
-              rows={(data && data.transactions) || []}
+              rows={generateRowsWithIndex(data || [])}
               columns={currentItems}
               rowCount={(data && data.total) || 0}
               rowsPerPageOptions={[20, 50, 100]}
